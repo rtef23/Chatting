7cd923f6ae2eb8f54df13f39676f64e2ddcc6447
rtef23 <rtef23@gmail.com>
2016-08-26 19:08:55 +0900

    Update, update router

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 var port = 3000;
 var express = require('express');
 var app = express();
+var router = require('./router/router')(app);
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
 
 var server = app.listen(port, 
 	function(){
