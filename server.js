574f10bfe4a03ee810340d7f75e4ecd5d7df006f
rtef23 <rtef23@gmail.com>
2016-09-10 17:24:56 +0900

    Update,  show http server ip address on console

--- server.js
+++ server.js
@@ -2,14 +2,35 @@ var port = 3000;
 var express = require('express');
 var session = require('express-session');
 var app = express();
+var os = require('os');
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
+exports.getWebServerIP = function () {
+	//return server's ip
+	var ifaces = os.networkInterfaces();
+	var result = '';
+	for (var dev in ifaces) {
+		var alias = 0;
+		ifaces[dev].forEach(function(details) {
+			if (details.family == 'IPv4' && details.internal === false) {
+				result = details.address;
+				++alias;
+			}
+		});
+	}
+
+	return result;
+}
+function getWebServerProtocol(){
+	return 'http';
+}
+
 var server = app.listen(port, 
 	function(){
-		console.log("this server run on port : " + port);
+		console.log("Web server address : " + getWebServerProtocol() + '://' + require('./server').getWebServerIP() + ':' + port);
 	}
 );
 
